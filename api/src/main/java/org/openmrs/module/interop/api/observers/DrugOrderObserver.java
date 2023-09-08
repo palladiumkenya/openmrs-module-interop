c96635eadb2a74d08917cb0edfd18596afa6f4e5
jecihjoy <jecihjoy@gmail.com>
2023-09-08 23:31:38 +0300

    Populate shr with patient medications

---
+++ api/src/main/java/org/openmrs/module/interop/api/observers/DrugOrderObserver.java
@@ -0,0 +1,56 @@
+package org.openmrs.module.interop.api.observers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hl7.fhir.r4.model.Medication;
+import org.openmrs.DrugOrder;
+import org.openmrs.Order;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.Daemon;
+import org.openmrs.event.Event;
+import org.openmrs.module.fhir2.api.translators.MedicationTranslator;
+import org.openmrs.module.interop.api.Subscribable;
+import org.openmrs.module.interop.api.metadata.EventMetadata;
+import org.openmrs.module.interop.utils.ObserverUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Slf4j
+@Component("interop.drugOrderObserver")
+public class DrugOrderObserver extends BaseObserver implements Subscribable<Order> {
+
+    @Autowired
+    private MedicationTranslator medicationTranslator;
+
+    @Override
+    public Class<?> clazz() {
+        return Order.class;
+    }
+
+    @Override
+    public List<Event.Action> actions() {
+        return ObserverUtils.defaultActions();
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        processMessage(message)
+                .ifPresent(metadata -> Daemon.runInDaemonThread(() -> prepareDrugMessage(metadata), getDaemonToken()));
+    }
+
+    private void prepareDrugMessage(@NotNull EventMetadata metadata) {
+        Order drugOrder = Context.getOrderService().getOrderByUuid(metadata.getString("uuid"));
+        if (drugOrder.getOrderType().getUuid().equals("131168f4-15f5-102d-96e4-000c29c2a5d7")) {
+            DrugOrder order = (DrugOrder) drugOrder;
+            Medication medication = medicationTranslator.toFhirResource(order.getDrug());
+            if (medication != null) {
+                this.publish(medication);
+            } else {
+                log.error("Couldn't find allergy with UUID {} ", metadata.getString("uuid"));
+            }
+        }
+    }
+}
